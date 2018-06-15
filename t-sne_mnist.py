3e43a9d0d34c31b357a3957d139629bb82d5595e
eipig <1543718673@qq.com>
2018-06-15 15:26:06 +0800

    add t-sne

---
+++ t-sne_mnist.py
@@ -0,0 +1,50 @@
+# 参考自https://blog.csdn.net/hustqb/article/details/78144384
+"""t-SNE对手写数字进行可视化"""
+from time import time
+import numpy as np
+import matplotlib.pyplot as plt
+import tensorflow as tf
+from tensorflow.examples.tutorials.mnist import input_data
+
+from sklearn import datasets
+from sklearn.manifold import TSNE
+
+
+def get_data():
+    mnist = input_data.read_data_sets('./MNIST_data', one_hot=False)
+    data = mnist.train.images
+    label = mnist.train.labels
+    n_samples, n_features = data.shape
+    return data, label, n_samples, n_features
+
+
+def plot_embedding(data, label, title):
+    x_min, x_max = np.min(data, 0), np.max(data, 0)
+    data = (data - x_min) / (x_max - x_min)
+
+    fig = plt.figure()
+    ax = plt.subplot(111)
+    for i in range(data.shape[0]):
+        plt.text(data[i, 0], data[i, 1], str(label[i]),
+                 color=plt.cm.Set1(label[i] / 10.),
+                 fontdict={'weight': 'bold', 'size': 9})
+    plt.xticks([])
+    plt.yticks([])
+    plt.title(title)
+    return fig
+
+
+def main():
+    data, label, n_samples, n_features = get_data()
+    print('Computing t-SNE embedding')
+    tsne = TSNE(n_components=2, init='pca', random_state=0)
+    t0 = time()
+    result = tsne.fit_transform(data)
+    fig = plot_embedding(result, label,
+                         't-SNE embedding of the digits (time %.2fs)'
+                         % (time() - t0))
+    plt.show(fig)
+
+
+if __name__ == '__main__':
+    main()
